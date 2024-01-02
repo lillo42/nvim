ae56e1fd31f2f439836361e5d9b11d2f4e4c8fb6
Rafael Andrade <rafael.andrade@justeattakeaway.com>
2024-01-02 13:39:28 +0000

    Improve configs

--- lua/configs/options.lua
+++ lua/configs/options.lua
@@ -1,4 +1,8 @@
 -- Setup color scheme
 vim.cmd.colorscheme('tokyonight')
 
+-- Setup fold method
+vim.opt.foldmethod = "expr"
+vim.foldexpr = "nvim_treesitter#foldexpr()"
+
 
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -109,11 +109,32 @@ return {
         ensure_installed = {
           "volar",
           "omnisharp",
-          "gopls"
+          "gopls",
+          "tsserver",
+          "angularls",
         },
         automatic_installation = true,
         handlers = {
           lsp_zero.default_setup,
+
+          omnisharp = function()
+            require("lspconfig").omnisharp.setup({
+              -- Enables support for roslyn analyzers, code fixes and rulesets.
+              enable_roslyn_analyzers = true,
+
+              -- Specifies whether 'using' directives should be grouped and sorted during
+              -- document formatting.
+              organize_imports_on_format = true,
+
+              -- Enables support for showing unimported types and unimported extension
+              -- methods in completion lists. When committed, the appropriate using
+              -- directive will be added at the top of the current file. This option can
+              -- have a negative impact on initial completion responsiveness,
+              -- particularly for the first few completion sessions after opening a
+              -- solution.
+              enable_import_completion = true,
+            })
+          end
         }
       })
     end
