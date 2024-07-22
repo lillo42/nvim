a974cc7f36f21557e72bf972389faa4969ec3add
Rafael Lillo <fine.bulgur_0i@icloud.com>
2024-07-22 16:24:20 +0100

    use lazy

--- init.lua
+++ init.lua
@@ -1,1 +1,2 @@
-require("configs")
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
--- lua/configs/init.lua
+++
@@ -1,5 +0,0 @@
-require("configs.set")
-require("configs.plugin")
-require("configs.autocmds")
-require("configs.options")
-require("configs.keymaps")
